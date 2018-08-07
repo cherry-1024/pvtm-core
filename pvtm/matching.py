170ce79ac198c1a89e2531d8652dfff8ca37c473
VikaNa <Viktoriia.Naboka@wirtschaft.uni-giessen.de>
2018-08-07 16:13:46 +0200

    updated layout

--- pvtm/matching.py
+++ pvtm/matching.py
@@ -22,7 +22,8 @@ ap.add_argument("-i", "--input", default="./Output",
 #                help="dash app port. Default = 8050")
 args = vars(ap.parse_args())
 
-assign= pd.read_csv(args['input']+"/comparison_results_JSE_RWE/matching/assign.csv")
+assign= pd.read_csv(args['input']+"/comparison_results_JSE_RWE/matching/assign.csv", index_col='RWE')
+assignment_cost=pd.read_csv(args['input']+"/comparison_results_JSE_RWE/matching/assignment_cost.csv")
 timeline_jse = pd.read_csv(args['input']+"/comparison_results_JSE_RWE/matching/timeline_overview_1.csv",index_col='Unnamed: 0')
 timeline_rwe = pd.read_csv(args['input']+"/comparison_results_JSE_RWE/matching/timeline_overview_2.csv",index_col='Unnamed: 0')
 
@@ -51,9 +52,14 @@ layout = html.Div(children=[
         ], className="six columns"),
     ], className="row"),
     html.Div([
-        html.H2('Timeline',style={'textAlign': 'center','color': '#1C4E80'} ),
-        dcc.Graph(id='timeline_4', animate=False,style={'background-color': '#7FDBFF'})
-    ]),
+        html.Div([
+            html.H2('Timeline',style={'textAlign': 'center','color': '#1C4E80'} ),
+            dcc.Graph(id='timeline_4', animate=False,style={'background-color': '#7FDBFF'})
+            ], className='six columns'),
+        html.Div([
+            html.H2(id='assignment_cost', style={'textAlign': 'center', 'color': '#1C4E80'})
+        ], className='six columns'),
+    ], className='row'),
     html.Div(id='matching-content')
 ])
 @app.callback(Output(component_id='explanation', component_property='children'),
@@ -61,8 +67,11 @@ layout = html.Div(children=[
               )
 def explanation(value):
     try:
-        value= float(value)
-        return 'Topic {} from RWE matches topic {} from JSE'.format(round(value),assign['JSE'][value])
+        if int(value)==27:
+            return 'There is no match for topic 27! Timelines did not update!'
+        else:
+            value= float(value)
+            return 'Topic {} from RWE matches topic {} from JSE.'.format(round(value),assign['JSE'][value])
     except Exception as e:
         with open(args['input'] + '/errors.txt', 'a') as f:
             f.write('Topic {}'.format(value))
@@ -106,9 +115,9 @@ def update_img_JSE(value):
 def update_timeline(topic):
     try:
         X = timeline_rwe.index  # timeline jse
-        Y = timeline_rwe[str(topic)].values
+        Y = timeline_rwe[str(topic)].ewm(span=5).mean().values
         X1 = timeline_jse.index # timeline rwe
-        Y1 = timeline_jse[str(assign['JSE'][float(topic)])].values
+        Y1 = timeline_jse[str(assign['JSE'][int(topic)])].ewm(span=5).mean().values
         figure = {
             'data': [
                 {'x': X, 'y': Y, 'type': 'line', 'name': 'RWE'},
@@ -125,3 +134,15 @@ def update_timeline(topic):
         with open(args['input'] + '/errors.txt', 'a') as f:
             f.write(str(e))
             f.write('\n')
+
+@app.callback(Output(component_id='assignment_cost', component_property='children'),
+              [Input(component_id='input-value_4', component_property='value')]
+              )
+def assigment_cost(value):
+    try:
+        return 'Assignment cost: {}'.format(round(assignment_cost.at[round(float(value)),str(assign['JSE'][float(value)])],4))
+    except Exception as e:
+        with open(args['input'] + '/errors.txt', 'a') as f:
+            f.write(str(e))
+            f.write('\n')
+
